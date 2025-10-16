f755569fdec2f1cf34def09803488cdfef08eb63
David-HernandezM <dh29550@gmail.com>
2025-10-16 13:10:38 -0600

    Added trait to ContractEvent

--- app/src/services/contract_service.rs
+++ app/src/services/contract_service.rs
@@ -4,7 +4,7 @@ use sails_rs::{
 };
 
 #[event]
-#[derive(Encode, TypeInfo)]
+#[derive(Encode, TypeInfo, Debug)]
 #[codec(crate = sails_rs::scale_codec)]
 #[scale_info(crate = sails_rs::scale_info)]
 pub enum ContractEvent {
